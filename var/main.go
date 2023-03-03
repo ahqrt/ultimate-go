0bf6bbc6f0b0c1e2fb110d806a18d33f22959344
ahqrt <1217223113@qq.com>
2023-03-03 17:38:04 +0800

    变量定义

---
+++ var/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//   1
+	var name = "zhangsan"
+	// 2
+	var age int = 312
+	// 3
+	gender := "male"
+
+	fmt.Println("name :", name, "age :", age, "gender :", gender)
+
+}
