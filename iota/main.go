b1b01bb92a4520c87e961b73a744fe1a02d3a87a
ahqrt <1217223113@qq.com>
2023-03-03 17:56:54 +0800

    chore: 匿名变量

--- iota/main.go
+++ iota/main.go
@@ -29,6 +29,16 @@ const (
 	iota简化了const类型的定义
 */
 
+func a() (int, bool) {
+	return 0, false
+}
+
 func main() {
 	fmt.Println(Err1, Err2)
+	// 定义匿名变量。 主要用处还是用于接受不想要的变量吧..., 因为不允许定义了变量不使用
+	var _ int
+	_, ok := a()
+	if ok {
+		//
+	}
 }
